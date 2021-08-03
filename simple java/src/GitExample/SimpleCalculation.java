77f7952317a394ec6b74fa3637cfefcef5fedbed
serveradminaccount <serveradminaccount@serveradminaccount-Latitude-5590>
2021-08-03 10:44:38 +0530

    first changes

--- simple java/src/GitExample/SimpleCalculation.java
+++ simple java/src/GitExample/SimpleCalculation.java
@@ -3,8 +3,7 @@ package GitExample;
 public class SimpleCalculation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello");
+		System.out.println("A");
 
 	}
 
