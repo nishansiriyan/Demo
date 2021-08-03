0859646e76207cd64a15d504ae2a6fe13a27062a
serveradminaccount <serveradminaccount@serveradminaccount-Latitude-5590>
2021-08-03 11:26:32 +0530

    changes made in code

--- simple java/src/GitExample/SimpleCalculation.java
+++ simple java/src/GitExample/SimpleCalculation.java
@@ -3,9 +3,8 @@ package GitExample;
 public class SimpleCalculation {
 
 	public static void main(String[] args) {
-		int num1=30;
-		int num2=40;
-		System.out.println(num1+num2);
+		String s="Anrun";
+		System.out.println(s);
 
 	}
 
