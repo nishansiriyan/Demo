1bfce05f403162c015a2194a8fb48dc1f89cc387
nishansiriyan <69184330+nishansiriyan@users.noreply.github.com>
2021-08-03 15:21:08 +0530

    Update SimpleCalculation.java
    Child commit

--- simple java/src/GitExample/SimpleCalculation.java
+++ simple java/src/GitExample/SimpleCalculation.java
@@ -3,7 +3,7 @@ package GitExample;
 public class SimpleCalculation {
 
 	public static void main(String[] args) {
-
+	//Addition
 	int number1=100;
 	int number2=200;
 	System.out.println(number1+number2);
