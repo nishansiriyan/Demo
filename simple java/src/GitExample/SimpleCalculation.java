cbbe00c226d0b155b17b284a06adcb4962abce58
serveradminaccount <serveradminaccount@serveradminaccount-Latitude-5590>
2021-08-03 09:58:00 +0530

    Added new package

---
+++ simple java/src/GitExample/SimpleCalculation.java
@@ -0,0 +1,11 @@
+package GitExample;
+
+public class SimpleCalculation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+
+	}
+
+}
