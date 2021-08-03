f2f0b6eb3d89d161ae7a8a2580d6b8239102e0b4
serveradminaccount <serveradminaccount@serveradminaccount-Latitude-5590>
2021-08-03 15:18:23 +0530

    local changes

--- simple java/src/GitExample/SimpleCalculation.java
+++ simple java/src/GitExample/SimpleCalculation.java
@@ -3,9 +3,8 @@ package GitExample;
 public class SimpleCalculation {
 
 	public static void main(String[] args) {
-
 	int number1=100;
-	int number2=200;
+	int number2=10;
 	System.out.println(number1+number2);
 	}
 
